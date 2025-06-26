3febb36c2751671493c70ac65b911cf5d6870552
Akula-Sonu <sonuakula28@gmail.com>
2025-06-27 00:33:23 +0530

    design-1

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const path = require('path');
 const Campground = require('./models/campground');
@@ -18,14 +19,22 @@ mongoose.connect('mongodb://127.0.0.1:27017/Staylo', {
 });
 
 
+
+
+app.engine('ejs',ejsMate);
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: true }));
-
-
-
-
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'/views'));
+
+
+
+
+
+
+
+
+
 
 app.get('/',(req,res)=>{
     res.render('home.ejs');
